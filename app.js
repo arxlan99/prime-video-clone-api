417f6e4f675bf54ad4532b7f2b9bf2fd5e092701
Özkan Arslan <ozkan.arslan.321@gmail.com>
2021-09-17 12:32:17 +0300

    Fix cors

--- app.js
+++ app.js
@@ -33,7 +33,10 @@ app.use((req, res, next) => {
     "Access-Control-Allow-Methods",
     "OPTIONS, GET, POST, PUT, PATCH, DELETE"
   );
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
   next();
 });
 
