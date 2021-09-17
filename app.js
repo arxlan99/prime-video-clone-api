77d08ff8eb935bab10b6aa89f2c9057da8cf43b3
Özkan Arslan <ozkan.arslan.321@gmail.com>
2021-09-17 12:22:08 +0300

    Add Cors access

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
 /*---- Routes -----*/
 app.use(authRoutes);
 app.use("/users", usersRoutes);
